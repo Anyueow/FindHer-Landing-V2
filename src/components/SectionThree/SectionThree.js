51747e1bc20eee88b62486128b8458258c271a2c
anyueow <anyushah@gmail.com>
2023-08-12 02:29:36 +0530

    fixing some styles

--- src/components/SectionThree/SectionThree.js
+++ src/components/SectionThree/SectionThree.js
@@ -3,7 +3,8 @@ import { Col, Container, Row } from "react-bootstrap";
 import image1 from "../../Assets/wholepic.png";
 import image2 from "../../Assets/insights.png";
 import image3 from "../../Assets/relevant.png";
-import image4 from "../../Assets/exclaim.png"
+import image4 from "../../Assets/exclaim.png";
+import "./secthree.css";
 
 function SectionThree() {
     const [isVisible, setIsVisible] = useState(false);
@@ -39,32 +40,26 @@ function SectionThree() {
     return (
         <section className="grid-section shade">
             <Container className="container-md">
-                <h1 className="main-name-sect mb-40">
-                    <strong>
-                        On Find
-                        <span className="text-wrapper">Her </span>
-                        you can get:
-                    </strong></h1>
-                <p className="section-description">Click below to  learn more about each feature. </p>
-                <Row className="grid-subsection card-section btn-section">
+
+                <Row className="btn-section">
                     <Col xs={12} md={4} className="tech-icons">
 
-                        <h2 className="card-head"> the whole picture </h2>
+                        <h2 className="card-head"> The whole picture </h2>
                         <div className="image-container">
                             <img src={image1} className="img-card" />  </div>
-                        <h3 className="cardSub"> comprehensive overviews
+                        <h3 className="cardSub"> Comprehensive overviews
                             of individual offices</h3>
                     </Col>
                     <Col xs={12} md={4} className="tech-icons">
 
-                        <h2 className="card-head"> <span className="text-deco1">exclusive</span> insights </h2>
+                        <h2 className="card-head"> <span className="text-deco1">Exclusive</span> insights </h2>
                         <div className="image-container">
                             <img src={image2} className="img-card" />  </div>
                         <h3 className="cardSub">real, transparent reviews from  women who have worked there</h3>
                     </Col>
                     <Col xs={12} md={4} className="tech-icons">
 
-                        <h2 className="card-head"> relevant to you <span><img src={image4} alt="exclaimtory" /></span></h2>
+                        <h2 className="card-head"> Relevant to you <span><img src={image4} alt="exclaimtory" /></span></h2>
                         <div className="image-container">
                             <img src={image3} className="img-card" />  </div>
                         <h3 className="cardSub"> Detailed  information on factors important to you </h3>
