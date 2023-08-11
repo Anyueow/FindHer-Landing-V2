7a18c54fde4d295c5681a6ef1a58dc3512decbee
anyueow <anyushah@gmail.com>
2023-08-12 02:55:34 +0530

    fixing some styles

--- src/components/SectionOne/Home2.js
+++ src/components/SectionOne/Home2.js
@@ -6,15 +6,15 @@ function Home2() {
         <div className="outer-container">
             <div className="inner-container">
                 <h1 className="main-name-sect">
-                    <strong>
+
                         <span className="text-wrapper">Job postings </span>
                         can be quite vague, and it's hard to figure out if a workplace
                         aligns with your requirements and values.
-                    </strong>
+
                 </h1>
                 <h1 className="main-sub">
              Sometimes, you’re left with
-                    <strong style={{fontWeight:"1000", color:"#ee2c5b"}}> more questions </strong>
+                    <strong style={{color:"#ee2c5b"}}> more questions </strong>
                     than answers!
                 </h1>
             </div>
