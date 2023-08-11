0e85def2ed1f7d4facd02573826962532b5a19f6
anyueow <anyushah@gmail.com>
2023-08-11 22:08:48 +0530

    Navbar.js fixed for deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,52 @@
-import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import logo from "../Assets/logo.png";
-import { Link } from 'react-router-dom';  // Import Link from react-router-dom
-
+import React, { useState, useEffect } from 'react';
+import logo from '../Assets/logo.png';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+    const [navColor, setNavColor] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
 
+    useEffect(() => {
+        const scrollHandler = () => {
+            if (window.scrollY >= 20) {
+                setNavColor(true);
+            } else {
+                setNavColor(false);
+            }
+        };
 
-    const [navColor, updateNavbar] = useState(false);
+        window.addEventListener("scroll", scrollHandler);
 
-    function scrollHandler() {
-        if (window.scrollY >= 20) {
-            updateNavbar(true);
-        } else {
-            updateNavbar(false);
-        }
-    }
-
-    window.addEventListener("scroll", scrollHandler);
+        return () => {
+            window.removeEventListener("scroll", scrollHandler);
+        };
+    }, []);
 
     return (
-        <Navbar
-            fixed="top"
-            expand="md"
-            className={navColor ? "sticky navbar-dark" : "navbar-dark"}
-            style={{ padding: '20%' }}
-        >
-            <Container fluid>
-                <Navbar.Brand href="/" className="d-flex">
-                    <img src={logo} className="img-fluid" alt="brand" style={{ maxWidth: '10%' }} />
-                </Navbar.Brand>
-                <Nav className="navbar-nav" defaultActiveKey="/">
-                    <Nav.Item>
-                        <Link to="/" className="nav-link">Home</Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Link to="/reviews_login" className="nav-link">Submit a Review!</Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Link to="/reviews_one" className="nav-link">For Businesses</Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Link to="/"  className="nav-link">Contact</Link>
-                    </Nav.Item>
-                </Nav>
-            </Container>
-        </Navbar>
+        <div className={`custom-navbar ${navColor ? 'navbar-colored' : ''}`}>
+            <div className="container">
+                <Link className="navbar-brand" to="/">
+                    <img src={logo} alt="brand" className="logo"/>
+                </Link>
+                <button className="navbar-toggler" onClick={() => setShowMenu(!showMenu)}>
+                    ☰
+                </button>
+                <ul className={`navbar-menu ${showMenu ? 'show' : ''}`}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/reviews_login">Submit a Review!</Link>
+                    </li>
+                    <li>
+                        <Link to="/reviews_one">For Businesses</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
