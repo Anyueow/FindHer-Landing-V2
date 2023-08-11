cb0c98dd3dfa4f5c00914465749ce3c7a8eb8c19
anyueow <anyushah@gmail.com>
2023-08-12 02:36:52 +0530

    fixing some styles

--- src/components/SectionThree/SectionThree.js
+++ src/components/SectionThree/SectionThree.js
@@ -3,107 +3,58 @@ import { Col, Container, Row } from "react-bootstrap";
 import image1 from "../../Assets/wholepic.png";
 import image2 from "../../Assets/insights.png";
 import image3 from "../../Assets/relevant.png";
-import image4 from "../../Assets/exclaim.png";
 import "./secthree.css";
 
 function SectionThree() {
-    const [isVisible, setIsVisible] = useState(false);
-    const [vis, setVis] = useState(false)
-    const [value, setValue] = useState(false)
-    const [showButton1, setShowButton1] = useState(true);
-    const [showButton2, setShowButton2] = useState(true);
-    const [showButton3, setShowButton3] = useState(true);
-    function toggleCard() {
-        setIsVisible(!isVisible);
-        setShowButton1(false);
-        setValue(false)
-        setVis(false)
-        setShowButton2(true);
-        setShowButton3(true);
+    const [selectedCard, setSelectedCard] = useState(null);
+
+    const images = [image1, image2, image3];
+    const cardTitles = ["The whole picture", "Exclusive insights", "Relevant to you"];
+    const cardSubtitles = [
+        "Comprehensive overviews of individual offices",
+        "real, transparent reviews from women who have worked there",
+        "Detailed information on factors important to you"
+    ];
+
+    const handleClick = (index) => {
+        setSelectedCard(selectedCard === index ? null : index);
     }
-    const toggleCard2 = () => {
-        setVis(!vis);
-        setIsVisible(false);
-        setShowButton2(false);
-        setShowButton1(true);
-        setShowButton3(true);
-    };
-    const toggleCard3 = () => {
-        setValue(!value);
-        setIsVisible(false);
-        setShowButton3(false);
-        setVis(false)
-        setShowButton2(true);
-        setShowButton1(true);
-    };
 
     return (
         <section className="grid-section shade">
             <Container className="container-md">
-
                 <Row className="btn-section">
-                    <Col xs={12} md={4} className="tech-icons">
-
-                        <h2 className="card-head"> The whole picture </h2>
-                        <div className="image-container">
-                            <img src={image1} className="img-card" alt ="check" />  </div>
-                        <h3 className="cardSub"> Comprehensive overviews
-                            of individual offices</h3>
-                    </Col>
-                    <Col xs={12} md={4} className="tech-icons">
-
-                        <h2 className="card-head"> <span className="text-deco1">Exclusive</span> insights </h2>
-                        <div className="image-container">
-                            <img src={image2} className="img-card" alt ="check"/>  </div>
-                        <h3 className="cardSub">real, transparent reviews from  women who have worked there</h3>
-                    </Col>
-                    <Col xs={12} md={4} className="tech-icons">
-
-                        <h2 className="card-head"> Relevant to you <span><img src={image4} alt="exclaimtory" /></span></h2>
-                        <div className="image-container">
-                            <img src={image3} className="img-card"  alt ="check"/>  </div>
-                        <h3 className="cardSub"> Detailed  information on factors important to you </h3>
-                    </Col>
+                    {images.map((img, index) => (
+                        <Col key={index} xs={12} md={4} className="tech-icons">
+                            <h2 className="card-head">{cardTitles[index]}</h2>
+                            <div className="image-container">
+                                <img src={img} className="img-card" alt={cardTitles[index]} />
+                            </div>
+                            <h3 className="cardSub">{cardSubtitles[index]}</h3>
+                        </Col>
+                    ))}
                 </Row>
+
                 <Row className="btn-section1">
-                    {
-                        showButton1 && (
-                                        <Col>
-                                            <button className="card-btn" onClick={toggleCard}><span className="card-btn-head">The Whole Picture</span></button>
-                                        </Col>)}
-                    {isVisible && (<div xs={12} md={4} className="tech-icons">
-                        <h2 className="card-head"> the whole picture </h2>
-                        <div className="image-container">
-                            <img src={image1} className="img-card" alt ="check" />  </div>
-                        <h3 className="cardSub"> comprehensive overviews
-                            of individual offices</h3>
-                    </div>)}
-                    {
-                        showButton2 && (<Col>
-                                        <button className="card-btn" onClick={toggleCard2}><span className="card-btn-head">Exclusive Insights</span></button>
-                                    </Col>)}
-                    {vis && (<div xs={12} md={4} className="tech-icons">
-                        <h2 className="card-head"> exclusive insights </h2>
-                        <div className="image-container">
-                            <img src={image2} className="img-card"  alt ="check"/>  </div>
-                        <h3 className="cardSub">real, transparent reviews from  women who have worked there</h3>
-                    </div>)}
-                    {
-                        showButton3 && (
-                                        <Col>
-                                            <button className="card-btn" onClick={toggleCard3}><spa className="card-btn-head">Tailored for You</spa></button>
-                                        </Col>)}
-                    {value && (<div xs={12} md={4} className="tech-icons">
-                        <h2 className="card-head"> relevant to you </h2>
-                        <div className="image-container">
-                            <img src={image3} className="img-card" alt ="check"/>  </div>
-                        <h3 className="cardSub"> Detailed  information on factors important to you </h3>
-                    </div>)}
+                    {cardTitles.map((title, index) => (
+                        <Col key={index} xs={12} md={4}>
+                            <button className="card-btn" onClick={() => handleClick(index)}>
+                                <span className="card-btn-head">{title}</span>
+                            </button>
+                            {selectedCard === index && (
+                                <div className="tech-icons">
+                                    <h2 className="card-head">{cardTitles[index]}</h2>
+                                    <div className="image-container">
+                                        <img src={images[index]} className="img-card" alt={cardTitles[index]} />
+                                    </div>
+                                    <h3 className="cardSub">{cardSubtitles[index]}</h3>
+                                </div>
+                            )}
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </section>
-
-
     );
 }
 
