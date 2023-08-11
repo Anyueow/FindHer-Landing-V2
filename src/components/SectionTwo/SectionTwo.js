e84fe1bc27d214503d2f0e24216d0ffb58bbf6e5
anyueow <anyushah@gmail.com>
2023-08-12 01:55:05 +0530

    Fixed SectionTwo for mobile & desktop!

--- src/components/SectionTwo/SectionTwo.js
+++ src/components/SectionTwo/SectionTwo.js
@@ -1,23 +1,22 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
-import base from "./Images/Untitled design-18.png";
-import ImgOne from "./Images/4.png";
-import ImgTwo from "./Images/5.png";
-import ImgThree from "./Images/Untitled design-17 1.png";
+import base from "./Images/maindemo.png";
 
+import "../Hero/home.css"
+import mobbase from "./Images/phone des.png";
 function sectTwo() {
     return (
-        <section>
-            <Container fluid id="home" className="full-height center-contents">
-                <Container className="home-content">
-                    <Row className="imgs-base2">
-                        <Col md={4} className="text-left"> {/* Align text to the left */}
-                            <div className="margin"><h1 className="main-name">
+        <section className="hero">
+            <Container className="content">
+                <Row className="hero-text">
+
+                    <Col md={5} xs={12}> {/* Align text to the left */}
+             <h1 className="main-name">
                                 <strong>That's why we're building <span style={{textDecoration:"underline",
-                                    textDecorationColor:"#ee2c5b"} }> FindHer </span>
+                                    textDecorationColor:"#ee2c5b", textDecorationThickness:"0.5rem"} }> FindHer</span>
                                 </strong>
-                            </h1> </div>
-                            <h3 className="Sub">
+                            </h1>
+                            <h3 className="description">
                                 We make it easy for women to access
                                 information on workplaces of interest, enabling more
                                 <span> informed decision-making </span>
@@ -27,16 +26,18 @@ function sectTwo() {
 
                         </Col>
 
-                        <Col md={8} >
-                            <img src={base} className="base2" alt="Findher Demo"/>
-                            <img src={ImgOne} className="hoverOne2" alt="Findher Demo"/>
-                            <img src={ImgTwo} className="hoverTwo2" alt="Findher Demo"/>
-                            <img src={ImgThree} className="hoverThree2" alt="Findher Demo"/>
+                        <Col md={7} xs ={12}>
+                            <img src={base} className="base-image desktop"
+                                 alt="Findher Demo"
+                            style={{marginLeft:"6rem"}}/>
+                            <img src={mobbase} className="base-image mobile" alt="Findher Demo"/>
+
+
                         </Col>
 
                     </Row>
                 </Container>
-            </Container>
+
             {/* You may include other sections/components below */}
         </section>
     );
