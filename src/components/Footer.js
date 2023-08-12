622a7447631494a998f8090839a977194fb15015
anyueow <anyushah@gmail.com>
2023-08-12 16:11:49 +0530

    bugs in footer fixed, deployed for phone

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,31 +9,31 @@ function Footer() {
   let year = date.getFullYear();
   return (
       <Container fluid id="contact" className="footer">
-        <Row>
-          <Col md="8" className="footer-copywright">
+        <Row className="marg justify-content-md-start">
+          <Col md="8" >
             <img src={logo} className="img-fluid" alt="brand" style={{ maxWidth: '15%' }} />
             <h2 className="Footerhed"> Envisioning an India where all women are engaged in fulfilling and financially rewarding work</h2>
             <h1 className="footer-copywright">
               Copyright © {year} FindHer</h1>
           </Col>
-          <Col md="4" className="Footerhed">
+          <Col md="4" >
             <h2 className="Footerhed"> <strong>
               Contact Us </strong></h2>
 
             <Row className="footer-icons">
-              <Col md={1}>
+              <Col md={1} xs="auto">
                 <a href="https://www.linkedin.com/in/soumyajit4419/"
                    style={{ color: "#EA394A"}} target="_blank" rel="noopener noreferrer">
                   <FaLinkedinIn />
                 </a>
               </Col>
-              <Col md={1}>
+              <Col md={1} xs="auto">
                 <a href="https://www.instagram.com/soumyajit4419"
                    style={{ color: "#EA394A" }} target="_blank" rel="noopener noreferrer">
                   <AiFillInstagram />
                 </a>
               </Col>
-              <Col md={1}>
+              <Col md={1} xs="auto">
                 <a href="https://www.instagram.com/soumyajit4419"
                    style={{ color: "#EA394A" }} target="_blank" rel="noopener noreferrer">
                   <AiFillMail />
