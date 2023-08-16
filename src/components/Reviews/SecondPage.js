19afa63eeee27f85aa9ac33ed4c16a5a967f34d3
anyueow <anyushah@gmail.com>
2023-08-17 00:13:48 +0530

    UI got fucked but functional backend

--- src/components/Reviews/SecondPage.js
+++ src/components/Reviews/SecondPage.js
@@ -1,28 +1,23 @@
 import React, { useState } from "react";
-import {Link, useNavigate} from 'react-router-dom';
-
-
+import { Link, useNavigate } from 'react-router-dom';
 import { Button, Container, Form, Row, Col } from "react-bootstrap";
 import "./reviewStyles.css";
-import { AiOutlineCalendar } from 'react-icons/ai';
 import NavbarContext from '../NavbarContext';
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export const SecondPage = () => {
-    const [companies, setCompany] = useState("");
-    const [Loc, setLoc] = useState("");
-    const [title, setTitle] = useState("");
-    const navbarHeight = React.useContext(NavbarContext);
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
+    const [companies, setCompany] = useState("");
 
+    const [Loc, setLoc] = useState("");
 
+    const [title, setTitle] = useState("");
 
-
-    // within your component
+    const navbarHeight = React.useContext(NavbarContext);
     const navigate = useNavigate();
-
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -33,17 +28,18 @@ export const SecondPage = () => {
             return;
         }
 
-        const selectedCompany = e.target.elements[0].value;
-        const selectedLocation = e.target.elements[1].value;
-        const selectedTitle = e.target.elements[2].value;
-        const selectedStartDate = e.target.elements[3].value;
-        const selectedEndDate = e.target.elements[4].value;
+        const formData = new FormData(e.target);
+        const selectedCompany = formData.get('companyName');
+        const selectedLocation = formData.get('companyOffice');
+        const selectedTitle = formData.get('positionTitle');
+        const selectedStartDate = formData.get('startDate');
+        const selectedEndDate = formData.get('endDate');
 
         const response = await fetch("http://localhost:3000/protectedRoute/createReview", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}` // Include JWT token in Authorization header
+                "Authorization": `Bearer ${token}`
             },
             body: JSON.stringify({
                                      companyName: selectedCompany,
@@ -55,40 +51,29 @@ export const SecondPage = () => {
         });
 
         if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            navigate('/reviews_three'); // Assuming you have a success page to navigate to
+            navigate('/reviews_three');
         } else {
             console.error(`Error: ${response.status} ${response.statusText}`);
         }
     }
 
-
-// Use this function in the Form onSubmit prop:
-// <Form onSubmit={handleSubmit} ...
-
     const companyNames = ["Company 1", "Company 2", "Company 3"];
     const companyLocs = ["Office 1", "Office 2"];
-    const titles =["Manager", "Associate", "Intern"];
-
-
-
+    const titles = ["Manager", "Associate", "Intern"];
+    const isFormValid = companies && title && startDate && endDate;
 
     return (
-        <Container className="container-second"
-                   style={{ paddingTop: navbarHeight }}>
-
-            <Row className="ROw" >
-
-                    <Form onSubmit={handleSubmit} className="form-grp-one">
-                        <h1 className="head-name" style={{marginBottom:"4%"}}>
+        <Container className="container-second" style={{ paddingTop: navbarHeight }}>
+            <Row className="ROw">
+                <Form onSubmit={handleSubmit} className="form-grp-one">
+                <h1 className="head-name" style={{marginBottom:"4%"}}>
                             Tell us about one <span style={{color: "#ee2c5b"}}> company </span>
                             you’ve worked at before : </h1>
                         <Row style={{marginBottom:"2%"}}>
                         <Col md={6} xs={12}>
                         <Form.Group>
                             <Form.Label>Company Name</Form.Label>
-                            <Form.Select  className="custom-input3" value={companies} onChange={e => setCompany(e.target.value)}>
+                            <Form.Select   value={companies} onChange={e => setCompany(e.target.value)}>
                                 {companyNames.map((companyName, index) => (
                                     <option key={index} value={companyName}>
                                         {companyName}
@@ -100,7 +85,8 @@ export const SecondPage = () => {
                         <Form.Group >
 
                             <Form.Label>Office Location</Form.Label>
-                            <Form.Select  className="custom-input3" value={Loc} onChange={e => setLoc(e.target.value)}>
+                            <Form.Select  value={Loc}
+                                          onChange={e => setLoc(e.target.value)}>
                                 {companyLocs.map((companyLoc, index) => (
                                     <option key={index} value={companyLoc}>
                                         {companyLoc}
@@ -109,107 +95,67 @@ export const SecondPage = () => {
                             </Form.Select>
                         </Form.Group> </Col></Row>
 
-                        <Row style={{marginBottom:"2%"}}>
-                        <Form.Group >
+                    <Row style={{marginBottom:"2%"}}>
+                        <Form.Group>
                             <Form.Label>Your Job Title</Form.Label>
-                            <Form.Select className="custom-input3"
-                                         style={{padding:"25px"}}
-                                         value={title}
-                                         onChange={e => setTitle(e.target.value)}>
-                                {titles.map((title, index) => (
-                                    <option key={index} value={title}>
-                                        {title}
+                            <Form.Select as="select"
+
+
+                                          style={{padding:"20px"}}
+                                          value={title}
+                                          onChange={e => setTitle(e.target.value)}>
+                                {titles.map((titleOption, index) => (
+                                    <option key={index} value={titleOption}>
+                                        {titleOption}
                                     </option>
                                 ))}
                             </Form.Select>
                         </Form.Group>
-                        </Row>
+                    </Row>
 
 
-                        <Row style={{marginBottom:"2%"}} className="justify-content-lg-start">
+                    <Row style={{marginBottom:"2%"}} className="justify-content-lg-start">
+                        <Col md={6} xs={6}>
+                            <Form.Group>
+                                <Form.Label>Start Date</Form.Label>
+                                <br/>
+                                <DatePicker
+                                    selected={startDate}
+                                    onChange={(date) => setStartDate(date)}
+                                    style={{ width: '400px' }} // Adding this line to set the width to 100%
 
-
-                            <Col md={6} xs={12}>
-                                <Form.Group >
-                            <Form.Label>Start Date</Form.Label>
-                            <div className="input-group date" id="datepickerStart" data-target-input="nearest">
-                                <input type="text"
-                                       className="form-control datetimepicker-input custom-input3"
-                                       data-target="#datepickerStart"/>
-                                <div className="input-group-append" data-target="#datepickerStart"
-                                     data-toggle="datetimepicker">
-                                    <div className="input-group-text"><AiOutlineCalendar /></div>
-                                </div>
-                            </div>
-                                </Form.Group>
+                                    className="form-control datetimepicker-input"
+                                    dateFormat="yyyy-MM-dd"
+                                />
+                            </Form.Group>
                         </Col>
-
-
-
-                            <Col md={6} xs={12} style={{marginTop:"-1%"}}>
-                                <Form.Group >
-                            <Form.Label className="mt-3">End Date</Form.Label>
-                                    <div className="input-group date" id="datepickerStart" data-target-input="nearest">
-                                        <input type="text"
-                                               className="form-control datetimepicker-input custom-input3"
-                                               data-target="#datepickerStart"
-                                               value={startDate} // Bind the input value to the state variable
-                                               onChange={(e) => setStartDate(e.target.value)} // Update the state variable on change
-                                        />
-                                        <div className="input-group-append" data-target="#datepickerStart"
-                                             data-toggle="datetimepicker">
-                                            <div className="input-group-text"><AiOutlineCalendar /></div>
-                                        </div>
-                                    </div>
-
-                                    <div className="input-group date" id="datepickerEnd" data-target-input="nearest">
-                                        <input type="text"
-                                               className="form-control datetimepicker-input custom-input3"
-                                               data-target="#datepickerEnd"
-                                               value={endDate} // Bind the input value to the state variable
-                                               onChange={(e) => setEndDate(e.target.value)} // Update the state variable on change
-                                        />
-                                        <div className="input-group-append" data-target="#datepickerEnd"
-                                             data-toggle="datetimepicker">
-                                            <div className="input-group-text"><AiOutlineCalendar /></div>
-                                        </div>
-                                    </div>
-
-
-                                </Form.Group>
-                            </Col>
-
-
-                        </Row>
-
-
-                        <Row>
-
-                            <Button className="button-sub"
-
-                                    style={{marginTop:"3%"}}
-                                    onClick={() => navigate('/reviews_three')}>
-
-                                Next </Button>
-
-                            <Link to="/reviews_two" className="nav-link2">
-
-                                <p > I have never worked anywhere before </p>
-                            </Link>
-
-                        </Row>
-
-
-
-
-                    </Form>
-
-
-
-
+                        <Col md={6} xs={6} style={{marginTop:"-1%"}}>
+                            <Form.Group>
+                                <Form.Label className="mt-3">End Date</Form.Label>
+                                <br/>
+                                <DatePicker
+                                    selected={endDate}
+                                    onChange={(date) => setEndDate(date)}
+                                    className="form-control datetimepicker-input "
+                                    dateFormat="yyyy-MM-dd"
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Button className="button-sub"
+                                type="submit"
+                                style={{ marginTop: "3%" }}
+                                disabled={!isFormValid}
+                        >
+                            Next
+                        </Button>
+                        <Link to="/reviews_two" className="nav-link2">
+                            <p> I have never worked anywhere before </p>
+                        </Link>
+                    </Row>
+                </Form>
             </Row>
-
-
         </Container>
     );
 };
